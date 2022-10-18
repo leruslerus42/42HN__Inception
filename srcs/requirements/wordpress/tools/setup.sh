c279b5fa6e0aff2944f7c3a9d8fc6bc4c28239c2
ruslan <ruslan.rajaobelina@gmail.com>
2022-10-18 05:01:28 +0200

    single containers

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [! -f "/home/www/wp-config.php"]; then
+	#wp core download --path=/home/wwww/
+	wget https://wordpress.org/latest.tar.gz
+	tar -xf latest.tar.gz
+	mv wordpress/* /home/www/
+	cd /home/www/
+	wp config create --dbname=$DB_NAME --dbuser=$DB_ADMIN \
+	--dbpass=$DB_ADMIN_PASS --dbhost=$DB_HOST
+	# wp db create
+	wp core install --url=$SITE_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN \
+	--admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-admin_email
+	# wp theme install twentyone
+	# wp theme activate twentyone
+
+	#setup redis cache
+	sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"	wp-config.php
+	wp plugin install redis-cache --activate
+	wp plugin update --all
+fi
+
+#enable redis cache on restart
+cd /home/www/
+wp redis enable
+
+#install adminer db manager
+if [ ! -f "/home/www/adminer.php" ]; then	
+	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php
+	mv adminer-4.8.1-mysql-en.php /home/www/adminer.php
+fi
+
+#configure timezone
+TIMEZONE="Europe/Helsinki"
+cp /usr/shar/zoneinfo/${TIMEZONE} /etc/localtime
+echo "${TIMEZONE}" > /etc/timezone
+sed -i "s|;*date.timezone =.*|date.timezone = ${TIMEZONE}|i" /etc/php8/php.ini
+
+#start php
+echo "Starting php-fpm"
+/usr/sbin/php-fpm8 -F -R
